e1e4d09fef2c653e67990f5100a0670e86aa91ad
Petrov Egor <petrovegor495@gmail.com>
2025-10-06 21:35:22 +0400

    решена задача 'Ёлочка (iostream)'

---
+++ 0/1/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "==================" << std::endl;
+    std::cout << "                   " << std::endl;
+    std::cout << " Happy New Year!  " << std::endl;
+    std::cout << "        *        " << std::endl;
+    std::cout << "        ~\\        " << std::endl;
+    std::cout << "       //o\\       " << std::endl;
+    std::cout << "      /o''o\\      " << std::endl;
+    std::cout << "        ||        " << std::endl;
+    std::cout << "                   " << std::endl;
+    std::cout << "==================" << std::endl;
+}
